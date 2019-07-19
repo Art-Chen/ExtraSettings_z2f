ad72e4b3b42e68e4ad2783ef2260a2bc99291a60
Art_Chen <Chenxy0201@qq.com>
2019-07-19 11:31:16 +0800

    Use new method to finish the process and add remove Preinstall app function

--- app/src/main/java/moe/chenxy/extrasettings/SettingFragment.java
+++ app/src/main/java/moe/chenxy/extrasettings/SettingFragment.java
@@ -47,45 +47,32 @@ public class SettingFragment extends PreferenceFragment {
         Preference resolution = getPreferenceManager()
                 .findPreference("resolution");
         resolution.setOnPreferenceClickListener(preference -> {
-            View view = getView();
-            String cmd1 = "wm size 1440x2560";
-            String cmd2 = "wm density 560";
-            ifroot = RootTools.isRootAvailable();
-
-            if (!ifroot) {
-                Snackbar snackbar = Snackbar.make(view, "Need Root!", Snackbar.LENGTH_LONG);
-                View snackbarView = snackbar.getView();
-                ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
-                ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
-                //设置Snackbar的背景色
-                //snackbarView.setBackgroundColor(Color.WHITE);
-                snackbar.show();
-            }
-            Command command1 = new Command(0, cmd1);
-            Command command2 = new Command(0, cmd2);
-            try {
-                RootTools.getShell(true).add(command1);
-                RootTools.getShell(true).add(command2);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (TimeoutException e) {
-                final Snackbar snackbar = Snackbar.make(view, "Get root timeout! Magisk may not run in background.", Snackbar.LENGTH_LONG);
-                View snackbarView = snackbar.getView();
-                ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
-                ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
-                snackbar.show();
-                e.printStackTrace();
-            } catch (RootDeniedException e) {
-                e.printStackTrace();
-                final Snackbar snackbar = Snackbar.make(view, "Please allow me to be root!", Snackbar.LENGTH_LONG);
-                View snackbarView = snackbar.getView();
-                ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
-                ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
-                snackbar.show();
-            }
+            changeResolution("1440x2160","560");
             return false;
         });
         // resolution done
+        // remove app
+        findPreference("removeapp").setOnPreferenceClickListener(preference -> {
+            removeApp("360PhoneAssist");
+            removeApp("iFlyIME");
+            removeApp("SogouBrowser");
+            removeApp("SohuNews");
+            removeApp("Weather");
+            removeApp("360cledroid");
+            final Snackbar snackbar = Snackbar.make(getView(), "Please restart your phone.", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            snackbar.setAction("Reboot", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rebootAdvanced(null);
+                }
+            });
+            snackbar.show();
+            return true;
+        });
+        // donate
         findPreference("wechat").setOnPreferenceClickListener(preference -> {
             donateWeixin();
             return true;
@@ -94,6 +81,18 @@ public class SettingFragment extends PreferenceFragment {
             donateAlipay("FKX03133TLJFCY8UNXHC56");
             return true;
         });
+        // donate done
+
+        // advanced reboot
+        findPreference("recovery").setOnPreferenceClickListener(preference -> {
+            rebootAdvanced("recovery");
+            return true;
+        });
+        findPreference("bootloader").setOnPreferenceClickListener(preference -> {
+            rebootAdvanced("bootloader");
+            return true;
+        });
+        // done
     }
 
     /**
@@ -128,4 +127,138 @@ public class SettingFragment extends PreferenceFragment {
         WeiXinDonate.saveDonateQrImage2SDCard(qrPath, BitmapFactory.decodeStream(weixinQrIs));
         WeiXinDonate.donateViaWeiXin(getActivity(), qrPath);
     }
+
+    /**
+     * 提供一个高级重启的接口
+     * @param arg 重启参数定义，arg为null时，普通重启。
+     */
+    public void rebootAdvanced(String arg) {
+        String cmd = "reboot " + arg;
+        Command command = new Command(0, cmd);
+        View view = getView();
+        try {
+            RootTools.getShell(true).add(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            final Snackbar snackbar = Snackbar.make(view, "Get root timeout! Magisk may not run in background.", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            snackbar.show();
+            e.printStackTrace();
+        } catch (RootDeniedException e) {
+            e.printStackTrace();
+            final Snackbar snackbar = Snackbar.make(view, "Please allow me to be root!", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            snackbar.show();
+        }
+    }
+    /**
+     * 分辨率修改接口
+     * @param resolution 分辨率 例如1440x2160
+     * @param density dpi
+     */
+    public void changeResolution(String resolution, String density){
+        View view = getView();
+        String cmd1 = "wm size " + resolution;
+        String cmd2 = "wm density " + density;
+        ifroot = RootTools.isRootAvailable();
+
+        if (!ifroot) {
+            Snackbar snackbar = Snackbar.make(view, "Need Root!", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            //设置Snackbar的背景色
+            //snackbarView.setBackgroundColor(Color.WHITE);
+            snackbar.show();
+        }
+        Command command1 = new Command(0, cmd1);
+        Command command2 = new Command(0, cmd2);
+        try {
+            RootTools.getShell(true).add(command1);
+            RootTools.getShell(true).add(command2);
+            final Snackbar snackbar = Snackbar.make(view, "Done!", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            snackbar.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            final Snackbar snackbar = Snackbar.make(view, "Get root timeout! Magisk may not run in background.", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            snackbar.show();
+            e.printStackTrace();
+        } catch (RootDeniedException e) {
+            e.printStackTrace();
+            final Snackbar snackbar = Snackbar.make(view, "Please allow me to be root!", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            snackbar.show();
+        }
+    }
+    /**
+     * 移除预装
+     * @param appname apk所在文件夹名字
+     */
+    public void removeApp(String appname){
+        View view = getView();
+        String cmd = "rm -rf /system/priv-app/" + appname;
+        String rwmount = "mount -o rw,remount /system";
+        ifroot = RootTools.isRootAvailable();
+
+        if (!ifroot) {
+            Snackbar snackbar = Snackbar.make(view, "Need Root!", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            //设置Snackbar的背景色
+            //snackbarView.setBackgroundColor(Color.WHITE);
+            snackbar.show();
+        }
+        Command command = new Command(0, cmd);
+        Command remount = new Command(0, rwmount);
+        try {
+            if (appname == null || appname.equals("")){
+                final Snackbar snackbar = Snackbar.make(view, "App Name is Null!", Snackbar.LENGTH_LONG);
+                View snackbarView = snackbar.getView();
+                ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+                ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+                snackbar.show();
+            } else {
+                RootTools.remount("/system","rw");
+                RootTools.getShell(true).add(remount);
+                RootTools.getShell(true).add(command);
+                final Snackbar snackbar = Snackbar.make(view, "Done!", Snackbar.LENGTH_LONG);
+                View snackbarView = snackbar.getView();
+                ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+                ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+                snackbar.show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            final Snackbar snackbar = Snackbar.make(view, "Get root timeout! Magisk may not run in background.", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            snackbar.show();
+            e.printStackTrace();
+        } catch (RootDeniedException e) {
+            e.printStackTrace();
+            final Snackbar snackbar = Snackbar.make(view, "Please allow me to be root!", Snackbar.LENGTH_LONG);
+            View snackbarView = snackbar.getView();
+            ((TextView) snackbarView.findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+            ((TextView) snackbarView.findViewById(R.id.snackbar_action)).setTextColor(Color.BLUE);
+            snackbar.show();
+        }
+    }
 }
+
